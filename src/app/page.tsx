76dd57c684b213d4ed256b8873bc06e2acf3f88f
Viktor <vitya.getman.79@gmail.com>
2024-10-14 15:52:22 +0200

    session middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,27 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+
+import { useCurrent } from "@/features/auth/hooks/use-current";
 import { Button } from "@/components/ui/button";
+import { useLogout } from "@/features/auth/hooks/use-logout";
 
 const Home: React.FC = () => {
+  const { data, isLoading } = useCurrent();
+  const router = useRouter();
+  const { mutate } = useLogout();
+
+  React.useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
   return (
     <div>
-      <Button variant="primary" size="lg">
-        Primary
-      </Button>
-      <Button variant="secondary" size="xs">
-        Secondary
-      </Button>
-      <Button variant="destructive">Destructive</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="muted">Muted</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="tertiary">Tertiary</Button>
+      ONLY FOR AUTH
+      <Button onClick={() => mutate()}>Logout</Button>
     </div>
   );
 };
