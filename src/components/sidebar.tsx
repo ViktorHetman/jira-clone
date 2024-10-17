85b47d591ac74e7970bf2b667e48d5fb9627fb8d
Viktor <vitya.getman.79@gmail.com>
2024-10-17 13:03:02 +0200

    workspaces switcher

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 import { DottedSeparator } from "./dotted-separator";
 import { Navigation } from "./navigation";
+import { WorkspaceSwitcher } from "./workspace-switcher";
 
 export const Sidebar: React.FC = () => {
   return (
@@ -10,6 +11,8 @@ export const Sidebar: React.FC = () => {
       <Link href="/">
         <Image src="/logo.svg" alt="logo" width={164} height={48}></Image>
       </Link>
+      <DottedSeparator className="my-4" />
+      <WorkspaceSwitcher />
       <DottedSeparator className="my-4" />
       <Navigation />
     </aside>
